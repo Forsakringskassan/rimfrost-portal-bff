2c628124397094a1a7d7b30d1908bb8d8fe0adff
LisaWedin_Ductus <lisa.wedin@ductus.se>
2026-06-02 12:22:23 +0200

    adds cors fix

--- src/main/java/se/fk/github/portalbff/model/TasksResponse.java
+++ src/main/java/se/fk/github/portalbff/model/TasksResponse.java
@@ -1,8 +1,10 @@
 package se.fk.github.portalbff.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public class TasksResponse
 {
+   @JsonProperty("operativa_uppgifter")
    public List<OperativUppgift> operativaUppgifter;
 }
