c1a7e3fd04edfdf3b41e503bbb6891803c42eed6
LisaWedin_Ductus <lisa.wedin@ductus.se>
2026-06-02 12:27:57 +0200

    fix: fixes typos

--- src/main/java/se/fk/github/portalbff/CorsFilter.java
+++ src/main/java/se/fk/github/portalbff/CorsFilter.java
@@ -47,10 +47,10 @@ public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilt
    private Response corsOkResponse(String origin)
    {
       return Response.ok()
-         .header("Access-Control-Allow-Origin", origin)
-         .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-         .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-         .header("Access-Control-Max-Age", "86400")
-         .build();
+            .header("Access-Control-Allow-Origin", origin)
+            .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+            .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+            .header("Access-Control-Max-Age", "86400")
+            .build();
    }
 }
